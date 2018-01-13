dbd370b3dccac8eed992226045586ed212319074
christophebedard <chrisvolkoff@gmail.com>
2018-01-13 16:59:09 -0500

    Add check for robot line crossings thingies

--- src/roomba_sim/sim_roombas.cpp
+++ src/roomba_sim/sim_roombas.cpp
@@ -17,8 +17,20 @@ static const std::string NODE_NAME = "sim_roombas"; /**< node name */
 static const double TARGET_ROBOT_RADIUS = 1.0; /**< circle radius for target robot initial position */
 static const double OBSTACLE_ROBOT_RADIUS = 5.0; /**<  circle radius for obstacle robot initial position */
 
-std::vector<Robot*> robots;
-Quad* quad;
+double arenaDimension;
+
+bool isTargetRobotOutsideArena(tf::Vector3 robotPosition) {
+    return !((robotPosition.getX() >= (-arenaDimension/2.0)) && (robotPosition.getX() <= (arenaDimension/2.0))
+             && (robotPosition.getY() >= (-arenaDimension/2.0)) && (robotPosition.getY() <= (arenaDimension/2.0)));
+}
+
+bool isTargetRobotAcrossGreenLine(tf::Vector3 robotPosition) {
+    return (robotPosition.getY() > (arenaDimension/2.0));
+}
+
+bool isTargetRobotAcrossRedLine(tf::Vector3 robotPosition) {
+    return (robotPosition.getY() < (-arenaDimension/2.0));
+}
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, NODE_NAME);
@@ -30,6 +42,9 @@ int main(int argc, char** argv) {
     int nbTargetRobots, nbObstacleRobots;
     n_p.getParam("/target_robot_count", nbTargetRobots);
     n_p.getParam("/obstacle_robot_count", nbObstacleRobots);
+    n_p.getParam("arena_dimension", arenaDimension);
+
+    std::list<std::unique_ptr<Robot>> robots;
 
     // create target robots
     for (int i = 0; i < nbTargetRobots; ++i) {
@@ -37,7 +52,8 @@ int main(int argc, char** argv) {
         double pos_y = TARGET_ROBOT_RADIUS * sin(i * (360 / nbTargetRobots) * DEG_TO_RAD);
         double yaw = i * (360 / nbTargetRobots) * DEG_TO_RAD;
         std::string color = (i%2 == 0) ? "red" : "green";
-        robots.push_back(new GroundRobot(n, i+1, tf::Vector3(pos_x, pos_y, 0.0), yaw, color));
+        std::unique_ptr<Robot> uptr(new GroundRobot(n, i+1, tf::Vector3(pos_x, pos_y, 0.0), yaw, color));
+        robots.push_back(std::move(uptr));
     }
 
     // create obstacle robots
@@ -47,11 +63,15 @@ int main(int argc, char** argv) {
         double pos_y = OBSTACLE_ROBOT_RADIUS * sin(i * (360 / nbObstacleRobots) * DEG_TO_RAD);
         double yaw = (-90.0 + (i * (360 / nbObstacleRobots))) * DEG_TO_RAD;
         std::string height = heights[i%4];
-        robots.push_back(new ObstacleRobot(n, i+1, tf::Vector3(pos_x, pos_y, 0.0), yaw, height));
+        std::unique_ptr<Robot> uptr(new ObstacleRobot(n, i+1, tf::Vector3(pos_x, pos_y, 0.0), yaw, height));
+        robots.push_back(std::move(uptr));
     }
 
     // create quad
-    quad = new Quad(n, rate.expectedCycleTime());
+    std::unique_ptr<Quad> quad(new Quad(n, rate.expectedCycleTime()));
+
+    // init sim
+    int nbTargetRobotsAcrossGreenLine = 0;
 
     // run simulation
     while (ros::ok())
@@ -60,24 +80,40 @@ int main(int argc, char** argv) {
         quad->update();
 
         // robots
-        for (auto& robotBase : robots) {
-            robotBase->update();
+        for (auto robotBaseIt = robots.begin(); robotBaseIt != robots.end(); ++robotBaseIt) {
+            (*robotBaseIt)->update();
 
-            std::string baseNS = robotBase->getNamespace();
+            // if robot is active
+            if ((*robotBaseIt)->isActive()) {
+                // if target robot
+                if ((*robotBaseIt)->getRobotType() == "ground") {
+                    // check robot bumper collisions
+                    for (auto robotIt = robots.begin(); robotIt != robots.end(); ++robotIt) {
+                        // if not same robot
+                        if (!(*(*robotIt) == *(*robotBaseIt))) {
+                            (*robotBaseIt)->checkRobotCollision((*robotIt)->getPosition());
+                        }
+                    }
 
-            // if target robot
-            if (robotBase->getRobotType() == "ground") {
-                // check robot bumper collisions
-                for (auto& robot : robots) {
-                    std::string robotNS = robot->getNamespace();
-                    // if not same robot
-                    if (baseNS != robotNS) {
-                        robotBase->checkRobotCollision(robot->getPosition());
+                    // check top interaction
+                    (*robotBaseIt)->checkTopInteraction(quad->getPosition(), quad->getInteractionDiameter());
+
+                    // check if robot crossed a line
+                    if (isTargetRobotOutsideArena((*robotBaseIt)->getPosition())) {
+                        // check which line was crossed
+                        if (isTargetRobotAcrossGreenLine((*robotBaseIt)->getPosition())) {
+                            ++nbTargetRobotsAcrossGreenLine;
+                            ROS_INFO_STREAM((*robotBaseIt)->getNamespace() + " crossed green line (total = " + std::to_string(nbTargetRobotsAcrossGreenLine) + ")");
+                        } else if (isTargetRobotAcrossRedLine((*robotBaseIt)->getPosition())) {
+                            ROS_INFO_STREAM((*robotBaseIt)->getNamespace() + " crossed red line");
+                        } else {
+                            ROS_INFO_STREAM((*robotBaseIt)->getNamespace() + " crossed white line");
+                        }
+
+                        // deactivate robot
+                        (*robotBaseIt)->deactivateRobot();
                     }
                 }
-
-                // check top interaction
-                robotBase->checkTopInteraction(quad->getPosition(), quad->getInteractionDiameter());
             }
         }
 
